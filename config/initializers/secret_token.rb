e8873ba2fae46d56f027603df5db4dfe09e747bb
monkegjinni <gngr.kck@gmail.com>
2013-02-18 11:51:26 +0200

    added some goodies to the gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contat::Application.config.secret_token = '6f40c65e4a6f9704f96d85129ada59eff7b2f7fd4fb01dd538b30ca16710c643f7fae8409dd7037f6343763a6225ce4d6b743a52aee09043a86ace27deebbbe0'
