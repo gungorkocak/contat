10cb5d1edfa68fc1ff373d740392b45d1e8551f9
monkegjinni <gngr.kck@gmail.com>
2013-02-21 00:58:03 +0200

    :baaad-commit: bunch of changes; added page based contacts request specs and implemented structure with half baked css. Refactored scss files as well.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,15 @@
 class ContactsController < ApplicationController
+  before_filter :authenticate_user_with_message!
+
   def index
+  end
+
+  protected
+
+  def authenticate_user_with_message!
+    unless user_signed_in?
+      flash[:notice] = I18n.t('pages.signin.flash.unauthorized')
+      redirect_to new_user_session_path
+    end
   end
 end
--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -5,11 +5,13 @@ module DeviseMacros
     let(:user) { FactoryGirl.create(:user) }
   end
 
-  def signin_user
+  def signin_user(username = nil, password = nil)
     visit new_user_session_path
-    fill_in "user_username", with: user.username
-    fill_in "user_password", with: user.password
+    fill_in "user_username", with: username || user.username
+    fill_in "user_password", with: password || user.password
     click_on I18n.t('pages.signin.submit') 
+
+    user
   end
  
   def signup_user(username = "cukubik", password = "nazdrovya", password_confirmation = nil)
@@ -19,5 +21,9 @@ module DeviseMacros
     fill_in "user_password", with: password
     fill_in "user_password_confirmation", with: password_confirmation
     click_on I18n.t('pages.signup.submit') 
+  end
+
+  def signout_user
+    click_on I18n.t('pages.sign_out_button')
   end
 end
